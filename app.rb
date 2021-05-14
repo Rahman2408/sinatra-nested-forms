41a4f65804c8dd4491348fd178b7db8660fbbafb
Abdelrahman Elsirafy <rahman2408@gmail.com>
2021-05-13 20:53:34 -0400

    commit tree 77a2918d5f98971519097b2b490ecb50e217e873

--- app.rb
+++ app.rb
@@ -4,6 +4,26 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get '/' do
+      erb :root
+    end
+
+    get '/new' do 
+      
+      erb :'pirates/new'
+    end
+
+    post '/pirates' do
+      add_pirate = params[:pirate]
+      @pirate= Pirate.new(add_pirate)
+
+      params[:pirate][:ships].each do |add_ship|
+          Ship.new(add_ship)
+      end
+      @ships= Ship.all
+
+    erb :'pirates/show'
+  end
 
   end
 end
